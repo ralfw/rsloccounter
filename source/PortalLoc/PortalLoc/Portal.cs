d7b2fbe749aa74ac925556876db981299fc636bc
martin mittermaier <martin.mittermaier@rohde-shwarz.com>
2014-05-27 14:11:51 +0200

    Portal

---
+++ source/PortalLoc/PortalLoc/Portal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rsloc.contracts.adapter;
+
+namespace PortalLoc
+{
+    public class Portal : IPortal
+    {
+        public void LOC_ausgeben(int loc)
+        {
+            Console.WriteLine("LOC: {0}", loc);
+        }
+
+        public string Quellenangabe_bestimmen()
+        {
+            var args = Environment.GetCommandLineArgs();
+
+            if (args.Length <= 1)
+            {
+                throw new ArgumentException("No command line arguments!");
+            }
+
+            return args[1];
+        }
+    }
+}
---
+++ source/PortalLoc/PortalLoc/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PortalLoc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rohde & Schwarz")]
+[assembly: AssemblyProduct("PortalLoc")]
+[assembly: AssemblyCopyright("Copyright © Rohde & Schwarz 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fed4e8ff-aedb-43d5-a08b-3df36306794e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
