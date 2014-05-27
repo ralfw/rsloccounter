e3c2096aca27bbb4504f34458aaf59fbc8722410
Thomas Deboben <t@deboben.de>
2014-05-27 14:43:01 +0200

    optimize tests

--- source/Domäne/DomäneTests/KommentarSchluckerTests.cs
+++ source/Domäne/DomäneTests/KommentarSchluckerTests.cs
@@ -6,45 +6,39 @@ namespace rsloc.Domäne.Tests
     using NUnit.Framework;
 
     using rsloc.contracts.daten;
-    using rsloc.contracts.domäne;
 
     [TestFixture()]
     public class KommentarSchluckerTests
     {
         private Quellcode quellcode = new Quellcode();
+        private Quellcode expectedResult = new Quellcode();
         private KommentarSchlucker schlucker = new KommentarSchlucker();
 
         [Test()]
         public void Leerzeilen_schluckenTest()
         {
-            string[] input = { "Eins", " ", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
-            string[] expexted = { "Eins", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
-            this.quellcode.Codezeilen = input;
-            this.quellcode = this.schlucker.Leerzeilen_schlucken(this.quellcode);
+            this.quellcode.Codezeilen = new[] { "Eins", " ", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
+            this.expectedResult.Codezeilen = new[] { "Eins", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
 
-            Assert.AreEqual(expexted, this.quellcode.Codezeilen);
+            Assert.AreEqual(this.expectedResult.Codezeilen, this.schlucker.Leerzeilen_schlucken(this.quellcode).Codezeilen);
         }
 
         [Test()]
         public void Mehrzeilige_Kommentare_schluckenTest()
         {
-            string[] input = { "Eins", " ", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
-            string[] expexted = { "Eins", " ", "// Zwei", "Drei", "Sieben" };
-            this.quellcode.Codezeilen = input;
-            this.quellcode = this.schlucker.Mehrzeilige_Kommentare_schlucken(this.quellcode);
+            this.quellcode.Codezeilen = new[] { "Eins", " ", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
+            this.expectedResult.Codezeilen = new[] { "Eins", " ", "// Zwei", "Drei", "Sieben" };
 
-            Assert.AreEqual(expexted, this.quellcode.Codezeilen);
+            Assert.AreEqual(this.expectedResult.Codezeilen, this.schlucker.Mehrzeilige_Kommentare_schlucken(this.quellcode).Codezeilen);
         }
 
         [Test()]
         public void Einzeilige_Kommentare_schluckenTest()
         {
-            string[] input = { "Eins", " ", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
-            string[] expexted = { "Eins", " ", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
-            this.quellcode.Codezeilen = input;
-            this.quellcode = this.schlucker.Einzeilige_Kommentare_schlucken(this.quellcode);
+            this.quellcode.Codezeilen = new[] { "Eins", " ", "// Zwei", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
+            this.expectedResult.Codezeilen = new[] { "Eins", " ", "Drei", "/* Vier", "Fünf", "Sechs */", "Sieben" };
 
-            Assert.AreEqual(expexted, this.quellcode.Codezeilen);
+            Assert.AreEqual(this.expectedResult.Codezeilen, this.schlucker.Einzeilige_Kommentare_schlucken(this.quellcode).Codezeilen);
         }
 
     }
