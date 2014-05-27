8825b670708cd69844dd8b97a3491c40b33fdf80
Thomas Deboben <t@deboben.de>
2014-05-27 13:32:35 +0200

    initial Domäne solution

---
+++ source/Domäne/Domäne/KommentarSchlucker.cs
@@ -0,0 +1,28 @@
+﻿namespace Domäne
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using rsloc.contracts.domäne;
+
+    public class KommentarSchlucker: IKommentarschlucker
+    {
+        public rsloc.contracts.daten.Quellcode Einzeilige_Kommentare_schlucken(rsloc.contracts.daten.Quellcode quellcode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public rsloc.contracts.daten.Quellcode Leerzeilen_schlucken(rsloc.contracts.daten.Quellcode quellcode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public rsloc.contracts.daten.Quellcode Mehrzeilige_Kommentare_schlucken(rsloc.contracts.daten.Quellcode quellcode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/Domäne/Domäne/Statistik.cs
@@ -0,0 +1,23 @@
+﻿namespace Domäne
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using rsloc.contracts.domäne;
+
+    public class Statistik: IStats
+    {
+        public void LOC_summieren(rsloc.contracts.daten.Quellcode quellcode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SummeLOC
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
